b3fa36dd3a656307e097e0e938f0aa22f2ef4e61
Gul Noor <67622150+gulnoor@users.noreply.github.com>
2024-05-26 02:33:45 +0500

    template 2.1 (fixed logger)

--- utils/logger.js
+++ utils/logger.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 function consoleLog(params) {
-  if (process.env.NODE_ENV === 'test') console.log(params);
+  if (process.env.NODE_ENV === 'development') console.log(params);
 }
 function errorLog(params) {
-  if (process.env.NODE_ENV === 'test') console.error(params);
+  if (process.env.NODE_ENV === 'development') console.error(params);
 }
 module.exports = { consoleLog, errorLog };
